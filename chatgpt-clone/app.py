d9ca149f62e61db9503970246ae27ccbf8c3f85b
Ricardo <ricardo.viana18@gmail.com>
2024-02-02 12:03:49 -0300

    Chatbot Simplificado

--- chatgpt-clone/app.py
+++ chatgpt-clone/app.py
@@ -9,22 +9,28 @@ from langchain.indexes import VectorstoreIndexCreator
 from langchain.chains import ConversationalRetrievalChain, RetrievalQA
 from langchain_community.vectorstores import Chroma
 
+from translate import Translator
+
 import constants
 
 # Set up OpenAI API key
 os.environ["OPENAI_API_KEY"] = constants.OPENAI_API_KEY
 
-loader = TextLoader('chatgpt-clone/processData/cnhRenovationProcess.txt', encoding="utf8")
-loader.load()
-index = VectorstoreIndexCreator().from_loaders([loader])
-
 system_message = {"role": "system", "content": "You are an agent of Detran of Paraiba\nYour taks is to always answer like a typical assistant to help people with doubts on some services in Detran\nAlways be kind and try to do your best to answer\n\nYour name is Detrinho, and you have a passion to help people.\nYour principal activity is Renovação de CNH."}
 
 css = ".wrapper {background: url('file=chatgpt-clone/images/detranBackgroudWhite.jpg'); background-size: cover; background-position: center}"
 
+translatorEnToPt = Translator(provider='libre', from_lang='en', to_lang='pt')
+
+translatorPtToEn = Translator(provider='libre', from_lang='pt', to_lang='en')
+
 with gr.Blocks(css=css) as demo:
-    chatbot = gr.Chatbot()
-    msg = gr.Textbox()
+    loader = TextLoader('chatgpt-clone/processData/cnhRenovationProcess.txt', encoding="utf8")
+    loader.load()
+    index = VectorstoreIndexCreator().from_loaders([loader])
+
+    chatbot = gr.Chatbot(label="Chatbot do Detran-PB")
+    msg = gr.Textbox(label="Caixa de texto", placeholder="Digite a sua dúvida")
     clear = gr.Button("Reiniciar conversa")
 
     state = gr.State([])
@@ -50,12 +56,18 @@ with gr.Blocks(css=css) as demo:
         )
         """
         #Carrega o langchain e aplica o modelo para responder de acordo com 
+        """ SE FOR USADO O MODELO COM CHATOPENAI, ELE MUDA MUITO DE CONTEXTO, APLICAS APLICANDO O LLM ELE SE DETEM AO BÁSICO, 
+        QUE É O QUE ESTÁ ESCRITO NO TXT, ELE POR EXEMPLO DIZ QUE A PESSOA DEVE CONSULTAR O SITE DO DETRAN, PARA MAIS INFORMAÇÕES INVÉS DE SÓ RESPONDER
+
         chain = ConversationalRetrievalChain.from_llm(llm=ChatOpenAI(model="gpt-3.5-turbo-1106"), 
         retriever = index.vectorstore.as_retriever(search_kwargs={"k":1}),
         )
         response = chain({"question": message, "chat_history": messages_history})
         messages_history.append((message, response['answer']))
         return response['answer'], messages_history
+        """
+        messages_history.append((message, index.query(message)))
+        return index.query(message), messages_history 
         #return response.choices[0].message.content, messages_history
 
     def init_history(messages_history):
