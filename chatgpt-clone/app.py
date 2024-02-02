765cdccf0e4cb0ad7e7e9ce7a1c083a5dae6a48c
Ricardo <ricardo.viana18@gmail.com>
2024-02-01 23:33:53 -0300

    first commit

---
+++ chatgpt-clone/app.py
@@ -0,0 +1,72 @@
+import gradio as gr
+import openai
+import time
+import os
+
+from langchain_community.document_loaders import TextLoader
+from langchain_community.chat_models import ChatOpenAI
+from langchain.indexes import VectorstoreIndexCreator
+from langchain.chains import ConversationalRetrievalChain, RetrievalQA
+from langchain_community.vectorstores import Chroma
+
+import constants
+
+# Set up OpenAI API key
+os.environ["OPENAI_API_KEY"] = constants.OPENAI_API_KEY
+
+loader = TextLoader('chatgpt-clone/processData/cnhRenovationProcess.txt', encoding="utf8")
+loader.load()
+index = VectorstoreIndexCreator().from_loaders([loader])
+
+system_message = {"role": "system", "content": "You are an agent of Detran of Paraiba\nYour taks is to always answer like a typical assistant to help people with doubts on some services in Detran\nAlways be kind and try to do your best to answer\n\nYour name is Detrinho, and you have a passion to help people.\nYour principal activity is Renovação de CNH."}
+
+css = ".wrapper {background: url('file=chatgpt-clone/images/detranBackgroudWhite.jpg'); background-size: cover; background-position: center}"
+
+with gr.Blocks(css=css) as demo:
+    chatbot = gr.Chatbot()
+    msg = gr.Textbox()
+    clear = gr.Button("Reiniciar conversa")
+
+    state = gr.State([])
+
+    def user(user_message, history):
+        return "", history + [[user_message, None]]
+
+    def bot(history, messages_history):
+        user_message = history[-1][0]
+        bot_message, messages_history = ask_gpt(user_message, messages_history)
+        #messages_history += [{"role": "assistant", "content": bot_message}]
+        history[-1][1] = bot_message
+        time.sleep(1)
+        return history, messages_history
+
+    def ask_gpt(message, messages_history):
+        #messages_history += [{"role": "user", "content": message}]
+        """
+        response = openai.chat.completions.create(
+            model="gpt-3.5-turbo-1106",
+            messages=messages_history,
+            top_p=1
+        )
+        """
+        #Carrega o langchain e aplica o modelo para responder de acordo com 
+        chain = ConversationalRetrievalChain.from_llm(llm=ChatOpenAI(model="gpt-3.5-turbo-1106"), 
+        retriever = index.vectorstore.as_retriever(search_kwargs={"k":1}),
+        )
+        response = chain({"question": message, "chat_history": messages_history})
+        messages_history.append((message, response['answer']))
+        return response['answer'], messages_history
+        #return response.choices[0].message.content, messages_history
+
+    def init_history(messages_history):
+        messages_history = []
+        messages_history.append((system_message,None))
+        return messages_history
+
+    msg.submit(user, [msg, chatbot], [msg, chatbot], queue=False).then(
+        bot, [chatbot, state], [chatbot, state]
+    )
+
+    clear.click(lambda: None, None, chatbot, queue=False).success(init_history, [state], [state])
+
+demo.launch(allowed_paths=list("chatgpt-clone/images/detranBackgroudWhite.jpg"))
